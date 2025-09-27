9ead7e0eebd85b6ca879a3d1f14032de44f3542a
Daniel Vitor Madeira Reis <147751321+Daniel-Vitor-infnet@users.noreply.github.com>
2025-09-27 19:29:50 -0300

    adjust

--- utils/crud.py
+++ utils/crud.py
@@ -5,16 +5,21 @@ def ler_arquivo():
     try:
         with open(ARQ, "r", encoding="utf-8") as arquivo:
             for linha in arquivo:
-                campos = linha.split(",")
+                campos = linha.strip().split(",")
                 
-                id = int(campos[0]) 
-                produto = campos[1] 
-                quantidade = int(campos[2])
-                preco = float(campos[3])
+                id, produto, quantidade, preco = int(campos[0]), campos[1], int(campos[2]), float(campos[3])
                 
                 produtos.append([id, produto, quantidade, preco])
     except Exception as ex:
-                print(f"Erro ao ler o arquivo: {ex}")
+        print(f"Erro ao ler o arquivo: {ex}")
     return produtos
 
-print(ler_arquivo())
+
+def salvar_arquivo(produtos):
+    try:
+        with open(ARQ, "w", encoding="utf-8") as arquivo:
+            for produto in produtos:
+                linha = f"{produto[0]},{produto[1]},{produto[2]},{produto[3]}\n"
+                arquivo.write(linha)
+    except Exception as ex:
+        print(f"Erro ao salvar o arquivo: {ex}")
